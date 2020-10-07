c79e23a15eadc911a3d3a8b76c59ee831d7394c8
Dirkve33 <68751006+Dirkve33@users.noreply.github.com>
2020-10-07 09:44:46 +0200

    Om te zien welke partijen geschikt zijn per dag

---
+++ GeschiktePartijen.sql
@@ -0,0 +1,43 @@
+SELECT 
+	Sub.*
+FROM
+(
+	SELECT 
+		DATEADD(HOUR, 2, HLM.HIS_TIMESTAMP)  Verplaatst,
+		HLM.HIS_ACTION  Actie,
+		LU.LogisticUnitID,
+		ISNULL(LU.LastConfirmedLocationID, C1.LastConfirmedLocationID)  Locatie,
+		SUBSTRING(ISNULL(LU.LastConfirmedLocationID, C1.LastConfirmedLocationID), 7, 2) Pad,
+		LU.LastConfirmedContainerID ContainerID,
+		ArticleID ArtikelNummer,
+		LUE.LotID,
+		Quantity Aantal
+
+
+	FROM wcs.LogisticUnits LU WITH (NOLOCK)
+	JOIN dbo.LogisticUnitExtensions LUE WITH (NOLOCK) ON LU.LogisticUnitID = LUE.LogisticUnitID
+	LEFT JOIN wcs.Containers C1 WITH (NOLOCK) ON LU.LastConfirmedContainerID = C1.ContainerID
+	LEFT JOIN um.users U WITH (NOLOCK) ON LU.ModifiedByUserID = U.Id
+	INNER JOIN (SELECT LUM.*,
+				ROW_NUMBER() 
+				  OVER ( 
+					partition BY LogisticUnitID 
+					ORDER BY HIS_TIMESTAMP DESC ) AS seqnum 
+				 FROM Hist.dbo_LogisticUnitMovements LUM
+				 WHERE CAST(HIS_TIMESTAMP AS DATE) = CAST(GETUTCDATE() AS DATE)) HLM 
+			 ON LU.LogisticUnitID = HLM.LogisticUnitID
+				AND seqnum = 1 
+
+	WHERE Identifiable = 0 and Quantity > 0
+	AND LEFT(ISNULL(LU.LastConfirmedLocationID, C1.LastConfirmedLocationID) , 4) = 'STWO'
+
+	AND HLM.HIS_ACTION = 'UPDATE'
+
+) Sub
+
+INNER JOIN (SELECT LotID, MAX(Quantity) Quantity
+			FROM WCS.LogisticUnits LUU WITH (NOLOCK)
+			JOIN dbo.LogisticUnitExtensions LUE WITH (NOLOCK)
+			ON LUU.LogisticUnitID = LUE.LogisticUnitID
+			GROUP BY LUE.LotID
+			) LUAGG ON Sub.Aantal = LUAGG.Quantity AND Sub.LotID = LUAGG.LotID
